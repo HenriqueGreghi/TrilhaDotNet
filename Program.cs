9951b4f36d0e83534f9b0745602c634a15af0dd0
HenriqueGreghi <92762708+HenriqueGreghi@users.noreply.github.com>
2022-08-04 11:09:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TrilhaDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            
+            Console.WriteLine($"A resposta da soma é:{soma}");
+
+
+                
+        }
+    }
+}
